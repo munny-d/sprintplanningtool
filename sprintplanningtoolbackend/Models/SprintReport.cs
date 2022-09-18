c36ae1f47070f0090230e9be4dde8c2b6fdf4ae9
Manpreet Dhillon <manpreet.dhillon04@currys.co.uk>
2022-09-18 02:03:07 +0100

    add sprint report model to backend

--- sprintplanningtoolbackend/Models/SprintReport.cs
+++ sprintplanningtoolbackend/Models/SprintReport.cs
@@ -1,13 +1,19 @@
 ﻿namespace sprintplanningtoolbackend.Models
 {
-    using System.Text.Json.Serialization;
-    public class User
+    public class SprintReport
     {
-        public int Id { get; set; }
-        public string? Username { get; set; }
-        public bool? IsAdmin { get; set; }
+        public DateTime SprintStartDate { get; set; }
+        public DateTime SprintEndDate { get; set; }
+        public List<User>? TeamMembers { get; set; }
+        public int? TeamSize { get; set; }
+        public int? AbsentDays { get; set; }
+        public int? WorkDays { get; set; }
+        public int? Capacity { get; set; }
+        public int? NewSP { get; set; }
+        public int? CarriedSP { get; set; }
+        public int? TotalSP { get; set; }
+        public string? SprintGoal { get; set; }
 
-        [JsonIgnore]
-        public string? PasswordHash { get; set; }
+        public string? CreatedByUser { get; set; }
     }
 }
